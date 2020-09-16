75522a72713eba781c810055a9806bdf93b7e4c3
Adam Marks <a.marks@techsmith.com>
2020-09-16 12:14:45 -0400

    Add UI Test

---
+++ UITests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "UITests" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "UITests" )]
+[assembly: AssemblyCopyright( "Copyright ©  2020" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "7f55414d-010d-4886-b674-e5c94f173c94" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
---
+++ UITests/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+using System.Reflection;
+
+namespace UITests
+{
+   [TestClass]
+   public class UnitTest1
+   {
+      protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:1000";
+
+      protected static WindowsDriver<WindowsElement> session;
+      
+      [TestInitialize]
+      public void Setup()
+      {
+         // Launch a new instance of wpf app
+         if (session == null)
+         {
+            string assemblyFolder = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location );
+            string WPFSameDirAppLocation = Path.Combine( assemblyFolder, @".\MyWPFTestApp.exe" );
+            // Create a new session to launch WPF application
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability( "app", WPFSameDirAppLocation );
+            session = new WindowsDriver<WindowsElement>( new Uri( WindowsApplicationDriverUrl ), appCapabilities );
+            Assert.IsNotNull( session );
+            Assert.IsNotNull( session.SessionId );
+
+            // Verify that WPF app is started
+            Assert.AreEqual( "MainWindow", session.Title );
+
+            // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
+            session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds( 1.5 );
+         }
+      }
+
+      [TestCleanup]
+      public void Cleanup()
+      {
+         // Close the application and delete the session
+         if (session != null)
+         {
+            session.Close();
+            session.Quit();
+            session = null;
+         }
+      }
+
+
+      [TestMethod]
+      public void CopyText_CopiesFromTextBoxToLabel()
+      {
+         WindowsElement textbox = session.FindElementByAccessibilityId( "textBox" );
+         WindowsElement copyTextButton = session.FindElementByAccessibilityId( "copyTextButton" );
+         WindowsElement label = session.FindElementByAccessibilityId( "label" );
+
+         Assert.IsNotNull( textbox );
+         Assert.IsNotNull( copyTextButton );
+         Assert.IsNotNull( label );
+
+         string testData = "Test123Test123";
+
+         // Select all text and delete to clear the edit box
+         textbox.SendKeys( Keys.Control + "a" );
+         textbox.SendKeys( Keys.Delete );
+         textbox.SendKeys( testData );
+         copyTextButton.Click();
+
+         Assert.IsTrue( label.Text.Equals( testData ) );
+      }
+   }
+}
